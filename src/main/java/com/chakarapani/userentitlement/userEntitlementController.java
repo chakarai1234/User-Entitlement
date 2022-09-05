2d2ec45334f115f12408fa6e5de2b83735288ddf
chakarai1234 <chakarai1234@gmail.com>
2022-09-05 21:27:34 +0800

    Added comments to the files

--- src/main/java/com/chakarapani/userentitlement/userEntitlementController.java
+++ src/main/java/com/chakarapani/userentitlement/userEntitlementController.java
@@ -16,25 +16,31 @@ import java.net.URI;
 import java.util.List;
 import java.util.UUID;
 
+// Suppress the raw types warning
+@SuppressWarnings("rawtypes")
+//Declaring that this is a RestController
 @RestController
+// Declaring the request mapping with value so that after the address the path of the API is declare
 @RequestMapping("/api/entitlement")
+// Slf4j is a logger
 @Slf4j
 public class userEntitlementController {
 
+    // Autowired the bean RestTemplate so that this is singleton
     @Autowired
     private RestTemplate restTemplate;
 
+    // Autowired the bean ObjectMapper so that this is singleton
     @Autowired
     private ObjectMapper objectMapper;
 
+    // Declare the endpoint after /api/endpoints/**
     @GetMapping("/")
     public List getEntitlement() throws IOException {
-//        user?username=chakarai1234
         RequestEntity<Void> request = RequestEntity.get(URI.create("http://USERS/api/users/all")).header("x" + "-correlation-id", UUID.randomUUID().toString()).header("country", "Singapore").build();
 
         String json = new RequestToJsonStringConverter(objectMapper, restTemplate).generateJsonNode(request, "data");
-        String correlation = new RequestToJsonStringConverter(objectMapper, restTemplate).generateJsonNode(request,
-                "x-correlation-id");
+        String correlation = new RequestToJsonStringConverter(objectMapper, restTemplate).generateJsonNode(request, "x-correlation-id");
 
         String CorrelationId = objectMapper.readValue(correlation, String.class);
         List users1 = objectMapper.readValue(json, List.class);
